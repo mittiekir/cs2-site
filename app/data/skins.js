954d28fbd9f12731a64f2a22cda5f77a05ab773d
mittiekir <mittiekir@gmail.com>
2026-04-26 21:27:14 +0300

    fix skins api

--- app/data/skins.js
+++ app/data/skins.js
@@ -1,4 +1,18 @@
 export async function GET() {
+  const extraNames = [
+    "AK-47 | Wild Lotus",
+    "AK-47 | Vulcan",
+    "AK-47 | Fire Serpent",
+    "AK-47 | Gold Arabesque",
+    "AK-47 | Case Hardened",
+    "M4A4 | Howl",
+    "M4A1-S | Welcome to the Jungle",
+    "AWP | Dragon Lore",
+    "AWP | Gungnir",
+    "AWP | Medusa",
+    "Desert Eagle | Blaze"
+  ];
+
   try {
     const res = await fetch(
       "https://bymykel.github.io/CSGO-API/api/en/skins.json",
@@ -7,25 +21,31 @@ export async function GET() {
 
     const data = await res.json();
 
-    const skins = data
-      .map((item) => {
-        const rawImage = item.image || item.icon_url || "";
+    const apiSkins = data
+      .map((item) => ({
+        name: item.name,
+        image: item.image || ""
+      }))
+      .filter((item) => item.name);
 
-        let image = rawImage;
+    const names = new Set(apiSkins.map((s) => s.name.toLowerCase()));
 
-        if (rawImage && !rawImage.startsWith("http")) {
-          image = `https://community.cloudflare.steamstatic.com/economy/image/${rawImage}`;
-        }
+    const extraSkins = extraNames
+      .filter((name) => !names.has(name.toLowerCase()))
+      .map((name) => ({
+        name,
+        image: ""
+      }));
 
-        return {
-          name: item.market_hash_name || item.name,
-          image
-        };
-      })
-      .filter((item) => item.name && item.image);
-
-    return Response.json({ skins });
+    return Response.json({
+      skins: [...extraSkins, ...apiSkins]
+    });
   } catch {
-    return Response.json({ skins: [] });
+    return Response.json({
+      skins: extraNames.map((name) => ({
+        name,
+        image: ""
+      }))
+    });
   }
 }
